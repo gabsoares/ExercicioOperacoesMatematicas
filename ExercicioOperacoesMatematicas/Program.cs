ccb627522dcbc808ca1bb2fa36b243fccf1ae705
gabsoares <soaresgabriel68@gmail.com>
2024-04-18 17:22:05 -0300

    Feature para possibilitar escolha do usuário

--- ExercicioOperacoesMatematicas/Program.cs
+++ ExercicioOperacoesMatematicas/Program.cs
@@ -1,26 +1,45 @@
 ﻿double num1, num2, soma, multiplicacao, divisao, subtracao;
+int escolha = 0;
 
 Console.WriteLine("Digite o primeiro numero: ");
 num1 = double.Parse(Console.ReadLine());
 Console.WriteLine("Digite o segundo numero: ");
 num2 = double.Parse(Console.ReadLine());
 
-soma = num1 + num2;
-subtracao = num1 - num2;
-multiplicacao = num1 * num2;
 
-if (num2 == 0)
+while (escolha == 0)
 {
-    Console.WriteLine("Nao divisivel por 0");
+    Console.WriteLine("Digite o numero: 1-Adição| 2-Subtração| 3- Multiplicacao| 4- Divisao");
+    escolha = int.Parse(Console.ReadLine());
+    if (escolha == 1)
+    {
+        soma = num1 + num2;
+        Console.WriteLine($"Soma: {soma}");
+    }
+    else if (escolha == 2)
+    {
+        subtracao = num1 - num2;
+        Console.WriteLine($"Subtracao: {subtracao}");
+    }
+    else if (escolha == 3)
+    {
+        multiplicacao = num1 * num2;
+        Console.WriteLine($"Multiplicacao: {multiplicacao}");
+    }
+    else
+    {
+        if (num2 == 0)
+        {
+            Console.WriteLine("Nao divisivel por 0");
+        }
+        else if (num2 < 0)
+        {
+            Console.WriteLine("Nao divisivel por numero negativo");
+        }
+        else
+        {
+            divisao = num1 / num2;
+            Console.WriteLine($"Divisao: {divisao}");
+        }
+    }
 }
-else if (num2 < 0)
-{
-    Console.WriteLine("Nao divisivel por numero negativo");
-}
-else
-{
-    divisao = num1 / num2;
-    Console.WriteLine($"Divisao: {divisao}");
-}
-
-Console.WriteLine($"Soma: {soma}\nSubtracao: {subtracao}\nMultiplicacao: {multiplicacao}");
