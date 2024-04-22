172d03e63a202c6cf0a00fe1ca860354281ed25f
gabsoares <soaresgabriel68@gmail.com>
2024-04-22 11:45:28 -0300

    Feature para inclusão do switch case.

--- ExercicioOperacoesMatematicas/Program.cs
+++ ExercicioOperacoesMatematicas/Program.cs
@@ -1,10 +1,10 @@
-﻿double num1 = 0, num2 = 0, num3 = 0, num4 = 0, resultado;
+﻿double num1 = 0, num2 = 0, num3 = 0, num4 = 0, resultado = 0;
 int escolha = 0;
 
 Console.Write("Digite o primeiro numero: ");
 num1 = double.Parse(Console.ReadLine());
 
-while(num2 == 0)
+while (num2 == 0)
 {
     Console.Write("Digite o segundo numero: ");
     num2 = double.Parse(Console.ReadLine());
@@ -26,31 +26,24 @@ while (escolha <= 0 || escolha > 4)
 {
     Console.Write("Digite o numero: 1-Adição| 2-Subtração| 3- Multiplicacao| 4- Divisao: ");
     escolha = int.Parse(Console.ReadLine());
-    if (escolha == 1)
+
+    switch (escolha)
     {
-        resultado = num1 + num2 + num3 + num4;
-        Console.WriteLine($"Soma: {resultado}");
-    }
-    else if (escolha == 2)
-    {
-        resultado = num1 - num2 - num3 - num4;
-        Console.WriteLine($"Subtracao: {resultado}");
-    }
-    else if (escolha == 3)
-    {
-        resultado = num1 * num2 * num3 * num4;
-        Console.WriteLine($"Multiplicacao: {resultado}");
-    }
-    else if (escolha == 4)
-    {
-        if (num2 < 0 || num3 < 0 || num4 < 0)
-        {
-            Console.WriteLine("Nao divisivel por numero negativo");
-        }
-        else
-        {
+        case 1:
+            resultado = num1 + num2 + num3 + num4;
+            break;
+        case 2:
+            resultado = num1 - num2 - num3 - num4;
+            break;
+        case 3:
+            resultado = num1 * num2 * num3 * num4;
+            break;
+        case 4:
             resultado = num1 / num2 / num3 / num4;
-            Console.WriteLine($"Divisao: {resultado}");
-        }
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
     }
 }
+Console.WriteLine("O resultado é: " + resultado);
